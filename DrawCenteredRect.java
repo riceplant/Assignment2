202f6b96feb0ba74568e44b8e6f321774c7d6785
CS106A <>
2018-06-15 15:18:33 +0200

    {"types":"C","runs":1}

--- DrawCenteredRect.java
+++ DrawCenteredRect.java
@@ -13,8 +13,8 @@ import java.awt.*;
 public class DrawCenteredRect extends GraphicsProgram {
 	
 	/** Size of the centered rect */
-	private static final int WIDTH = 350;
-	private static final int HEIGHT = 270;
+	private static final int WIDTH = 300;
+	private static final int HEIGHT = 220;
 
 	public void run() {
 		double x = (getWidth() - WIDTH) / 2; // getting the x value which will center the rectangle in the middle of the screen
