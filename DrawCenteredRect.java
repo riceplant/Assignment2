a1f7da4b8df2c25ad2ccfbf962846dd756e97d30
CS106A <>
2018-06-15 15:04:49 +0200

    {"types":"C","runs":0}

--- DrawCenteredRect.java
+++ DrawCenteredRect.java
@@ -17,7 +17,7 @@ public class DrawCenteredRect extends GraphicsProgram {
 	private static final int HEIGHT = 270;
 
 	public void run() {
-		
+		GRect rect = new GRect(x, y, WIDTH, HEIGHT);
 	}
 }
 
