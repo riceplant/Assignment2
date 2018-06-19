5531994268ac2de2dc68d55fa29808dc87a2fe91
CS106A <>
2018-06-19 12:20:47 +0200

    {"types":"C","runs":4}

--- FindRange.java
+++ FindRange.java
@@ -21,6 +21,7 @@ public class FindRange extends ConsoleProgram {
 			} 
 			println("Smallest: " + min);
 			println("Largest: " + max);
+			break;
 		}		
 	}
 	/* Specifies the value of the sentinel */
