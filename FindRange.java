ca3767dfde7c3ee9895d4bd25e3a39d31ac9f305
CS106A <>
2018-06-22 14:42:02 +0200

    {"types":"C","runs":6}

--- FindRange.java
+++ FindRange.java
@@ -15,23 +15,8 @@ public class FindRange extends ConsoleProgram {
 		int max = 0;
 		int min = 0;
 		println("This program finds the largest and smallest number.");
-		while (true) {
-			int input = readInt("? ");
-			while (input != SENTINEL) {
-				input = readInt("? ");
-				if (input == SENTINEL) {
-					println("Please use 0 to end the user input.");
-					break;
-				}
-				if (input != SENTINEL && input > min && input > max) {
-					max = input;
-					min = input;
-				}
-			}
-			println("Smallest: " + min);
-			println("Largest: " + max);
-			break;
-		}
+		
+		
 	}
 	private static final int SENTINEL = 0;
 }
