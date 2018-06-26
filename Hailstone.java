18cf14b0c86d8c2f551c47cbcd2b9504f4f8f84e
CS106A <>
2018-06-26 12:23:14 +0200

    {"types":"C","runs":14}

--- Hailstone.java
+++ Hailstone.java
@@ -10,7 +10,7 @@ import acm.program.*;
 
 public class Hailstone extends ConsoleProgram {
 	public void run() {
-		int n = readLine("Enter a number: ");
+		int n = readInt("Enter a number: ");
 		
 		/* While n is not 1 loop through the whole program to get to 1 */
 		while (n != 1) {
