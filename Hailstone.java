6adf9526b1dd761c42b7dd962ea12432e4d3e8f5
CS106A <>
2018-06-27 11:17:56 +0200

    {"types":"C","runs":3}

--- Hailstone.java
+++ Hailstone.java
@@ -21,18 +21,29 @@ public class Hailstone extends ConsoleProgram {
 					int nOdd = 3 * n + 1;
 					println(n + " is odd, so I make 3n + 1: " + nOdd);
 					n = nOdd;
+					break;
 				}
 				while (n % 2 == 0) {
 					int nEven = n / 2;
 					println(n + " is even so I take half: " + nEven);
 					nEven = n;
+					break;
 				}
 				
 			}
 			if (n % 2 == 0) {
-				int nEven = n / 2;
-				println(n + " is even so I take half: " + nEven);
-				n = nEven;
+				while (n % 2 == 0) {
+					int nEven = n / 2;
+					println(n + " is even so I take half: " + nEven);
+					nEven = n;
+					break;
+				}
+				while (n % 2 != 0) {
+					int nOdd = 3 * n + 1;
+					println(n + " is odd, so I make 3n + 1: " + nOdd);
+					n = nOdd;
+					break;
+				}
 			}
 		}
 	}
