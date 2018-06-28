9d85d9a7ae1244cab8f58855873d6d09ac9921e2
CS106A <>
2018-06-28 17:00:54 +0200

    {"types":"C","runs":8}

--- Hailstone.java
+++ Hailstone.java
@@ -14,9 +14,16 @@ public class Hailstone extends ConsoleProgram {
 		int counter = 0;
 		int n = readInt("Enter a number: ");
 		
-		/* While n is not 1 loop through the whole program to get to 1 */
 		while (true) {
+			
+			/* While n is not 1 loop through the whole program to get to 1 */
 			while (n != 1) {
+				
+				/* If n is odd we store the value which results from 3n+1 in nTempOdd to
+				 * get the new number to be output in the string. Afterwards we store
+				 * nTempOdd into n and break out the while loop to check if the new number
+				 * is Odd or Even. Same goes for odd numbers.
+				 * While we're doing this we keep track of the process using counter++*/
 				if (n % 2 != 0) {
 					int nTempOdd = n * 3 + 1;
 					println(n + " is odd, so I make 3n+1: " + nTempOdd);
