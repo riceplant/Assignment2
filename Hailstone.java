1565776036d4862d574a6db258a63d021930037e
CS106A <>
2018-06-28 16:36:00 +0200

    {"types":"C","runs":0}

--- Hailstone.java
+++ Hailstone.java
@@ -14,16 +14,21 @@ public class Hailstone extends ConsoleProgram {
 		int n = readInt("Enter a number: ");
 		
 		/* While n is not 1 loop through the whole program to get to 1 */
-		while (n != 1) {
-			if (n % 2 != 0) {
-				int nTempOdd = n * 3 + 1;
-				println(n + " is odd, so I make 3n+1: " + nTempOdd);
-				n = nTempOdd;
+		while (true) {
+			while (n != 1) {
+				if (n % 2 != 0) {
+					int nTempOdd = n * 3 + 1;
+					println(n + " is odd, so I make 3n+1: " + nTempOdd);
+					n = nTempOdd;
+					break;
+				} else if (n % 2 == 0) {
+					int nTempEven = n / 2;
+					println(n + "is even so I take half: " + nTempEven);
+					n = nTempEven;
+					break;
+				}
+			} if (n == 1) {
 				break;
-			} else if (n % 2 == 0) {
-				int nTempEven = n / 2;
-				println(n + "is even so I take half: " + nTempEven);
-				n = nTempEven;
 			}
 		}
 	}
