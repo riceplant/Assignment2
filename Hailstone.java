473a3c65a17cbcf5bf532d3bc21ac3b4814ae10d
CS106A <>
2018-06-28 16:41:06 +0200

    {"types":"C","runs":3}

--- Hailstone.java
+++ Hailstone.java
@@ -11,6 +11,7 @@ import acm.program.*;
 
 public class Hailstone extends ConsoleProgram {
 	public void run() {
+		int counter;
 		int n = readInt("Enter a number: ");
 		
 		/* While n is not 1 loop through the whole program to get to 1 */
@@ -20,6 +21,7 @@ public class Hailstone extends ConsoleProgram {
 					int nTempOdd = n * 3 + 1;
 					println(n + " is odd, so I make 3n+1: " + nTempOdd);
 					n = nTempOdd;
+					counter++;
 					break;
 				} else if (n % 2 == 0) {
 					int nTempEven = n / 2;
