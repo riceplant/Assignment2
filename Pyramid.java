78cd6e318bb6a236e06c77ba4a6a6038761fa915
CS106A <>
2018-07-04 15:30:35 +0200

    {"types":"C","runs":22}

--- Pyramid.java
+++ Pyramid.java
@@ -37,9 +37,9 @@ public class Pyramid extends GraphicsProgram {
 		add(bricket);
 		
 		/** set the x-coordinate that it will be at the half of the bricket*/
-		double x2 = (getWidth() / 2 - BRICK_WIDTH); 
+		double x2 = (getWidth() - BRICK_WIDTH * 2) / 2; 
 		
-		/** set the y-coordinate that it will be at the bottom of the bricket*/
+		/** set the y-coordinate that it will be set at the bottom of the bricket*/
 		double y2 = (getHeight() / 2 - BRICK_HEIGHT);
 		
 		GRect bricket2 = new GRect(x2, y2, BRICK_WIDTH, BRICK_HEIGHT);
