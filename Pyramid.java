64ab65321e4e27e13433d96415e3f32503b4fa39
CS106A <>
2018-07-05 11:55:02 +0200

    {"types":"C","runs":8}

--- Pyramid.java
+++ Pyramid.java
@@ -46,8 +46,8 @@ public class Pyramid extends GraphicsProgram {
 		bricket.setFilled(true);
 		add(bricket2);
 		
-		for (int i = 2; i < BRICKS_IN_BASE; i++) {
-			for (int j = 3; j < BRICKS_IN_BASE; j++) {
+		for (int i = 2; i < BRICKS_IN_BASE; i =+ 2) {
+			for (int j = 3; j < BRICKS_IN_BASE; j =+ 3) {
 				double x3 = (getWidth() - BRICK_WIDTH * i) / 2;
 				double y3 = (getHeight() - BRICK_HEIGHT * j) / 2;
 				GRect brick3 = new GRect (x3, y3, BRICK_WIDTH, BRICK_HEIGHT);
